e90825206fe46f8824a0b40bb16b5ab660207063
RujiraKongsomran <rujirabiwberry@gmail.com>
2021-08-31 08:24:17 +0700

    Setup RecyclerView

--- app/src/main/java/com/rujirakongsomran/restaurantfooddeliveryapp/MainActivity.java
+++ app/src/main/java/com/rujirakongsomran/restaurantfooddeliveryapp/MainActivity.java
@@ -1,14 +1,22 @@
 package com.rujirakongsomran.restaurantfooddeliveryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 
 import android.os.Bundle;
 import android.view.View;
 
+import com.rujirakongsomran.restaurantfooddeliveryapp.adapter.BestForYouAdapter;
 import com.rujirakongsomran.restaurantfooddeliveryapp.databinding.ActivityMainBinding;
+import com.rujirakongsomran.restaurantfooddeliveryapp.model.BestForYou;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    BestForYouAdapter bestForYouAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +25,15 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 //        setContentView(R.layout.activity_main);
+    }
+
+    private void setBestForYouRecyclerView(List<BestForYou> bestForYouList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(
+                this,
+                RecyclerView.HORIZONTAL, false);
+        binding.recyclerview.setLayoutManager(layoutManager);
+        bestForYouAdapter = new BestForYouAdapter(this, bestForYouList);
+        binding.recyclerview.setAdapter(bestForYouAdapter);
     }
 }
